5dd0699ad012b655dcf7c76fb097fa06571f8b86
Rafael <rafaelramosdeandrade20@gmail.com>
2023-10-13 17:41:05 -0300

    style: organizando nomes de variáveis e cometários.

--- include/graphs.h
+++ include/graphs.h
@@ -13,9 +13,10 @@
 	 * @brief Struct vértice.
 	 */
 	struct gvertex{
-		unsigned id; 	  // id Identificador do vértice.
-		unsigned vdegree; // Grau do vértice.
-		void *obj;	  // Objeto armazenado no vértice.
+		unsigned id; 	 	// Identificador do vértice.
+		unsigned vdegree;	// Grau do vértice.
+		unsigned obj_size;	// Tamanho do objeto.
+		    void *obj;	  	// Objeto armazenado no vértice.
 		
 	}
 
@@ -23,24 +24,37 @@
 	 * @brief Struct arestas.
 	 */
 	struct gedge{
-		struct gvertex *vert_right; // Vértice a direita.
-		struct gvertex *vert_left;  // Vértice a esquerda.
-		unsigned weight;	    // Peso do vértice
+		unsigned weight;	     	// Peso da aresta.
+		  struct gvertex *vert_right;	// Vértice a direita.
+		  struct gvertex *vert_left; 	// Vértice a esquerda.
 	}
 
 	/**
 	 * @brief Struct grafo.
 	 */
-	struct graph{
-		unsigned gdegree; // Grau do grafo.
-		unsigned tvertex; // Total de vertíces.
-		unsigned tedge;	  // Total de arestas.
-		bool directed;	  // Se o grafo é direcionado ou não.
+	struct ggraph{
+		unsigned gdegree; 		// Grau do grafo.
+		unsigned vertex_total; 		// Total de vertíces.
+		unsigned edge_total;	  	// Total de arestas.
+		    bool directed;	  	// Se o grafo é direcionado ou não.
 
+		  struct list *vertex_table;	// Tabela de vértices;
+		  struct list *edge_table;	// Tabela de arestas.
 	}
 	
 	typedef struct gvertex;
 	typedef struct gedge;
-	
+	typedef struct ggraph;
 
+	ggraph * create_graph(gvertex *vertex, bool directed, ){
+		ggraph *graph = malloc(sizeof(ggraph));
+		
+		graph->gdegree = 0;
+		graph->total_vertex = 0;
+		graph->total_edge = 0;
+		
+		graph->vertex_table = create_list(vertex);
+	}
+
+	
 #endif
