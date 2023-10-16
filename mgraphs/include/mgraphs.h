a61a18ab0235bb8062d453f2f20e92afc0245763
Rafael <rafaelramosdeandrade20@gmail.com>
2023-10-16 16:55:56 -0300

    refactor: simplificando objetivo inicial

--- mgraphs/include/mgraphs.h
+++ mgraphs/include/mgraphs.h
@@ -7,54 +7,21 @@
 #define GRAPHS_H
 
 	#include <stdlib.h>
-	#include <bool.h>
-
-	/**
-	 * @brief Struct vértice.
-	 */
-	struct gvertex{
-		unsigned id; 	 	// Identificador do vértice.
-		unsigned vdegree;	// Grau do vértice.
-		unsigned obj_size;	// Tamanho do objeto.
-		    void *obj;	  	// Objeto armazenado no vértice.
-		
-	}
-
-	/**
-	 * @brief Struct arestas.
-	 */
-	struct gedge{
-		unsigned weight;	     	// Peso da aresta.
-		  struct gvertex *vert_right;	// Vértice a direita.
-		  struct gvertex *vert_left; 	// Vértice a esquerda.
-	}
-
+	#include <stdbool.h>
+	
 	/**
 	 * @brief Struct grafo.
 	 */
-	struct ggraph{
+	struct{
 		unsigned gdegree; 		// Grau do grafo.
-		unsigned vertex_total; 		// Total de vertíces.
-		unsigned edge_total;	  	// Total de arestas.
+		unsigned total_vertex; 		// Total de vertíces.
+		unsigned total_edge;	  	// Total de arestas.
 		    bool directed;	  	// Se o grafo é direcionado ou não.
-
-		  struct list *vertex_table;	// Tabela de vértices;
-		  struct list *edge_table;	// Tabela de arestas.
-	}
+		     int *matrix ;		// Linha de arestas.
+	}typedef ggraph;
 	
-	typedef struct gvertex;
-	typedef struct gedge;
-	typedef struct ggraph;
 
-	ggraph * create_graph(gvertex *vertex, bool directed, ){
-		ggraph *graph = malloc(sizeof(ggraph));
-		
-		graph->gdegree = 0;
-		graph->total_vertex = 0;
-		graph->total_edge = 0;
-		
-		graph->vertex_table = create_list(vertex);
-	}
+	
 
 	
 #endif
