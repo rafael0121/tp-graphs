41bfce7d6d4089e99bb415fb099e4bfcefb4ada6
Rafael <rafaelramosdeandrade20@gmail.com>
2023-10-17 16:19:27 -0300

    fix: aresta direcionada não era diferenciada

--- mgraphs/src/mgraphs.c
+++ mgraphs/src/mgraphs.c
@@ -42,7 +42,7 @@ void edge_insert (ggraph *graph, unsigned id1, unsigned id2, int weight){
 		graph->edge_array[edge_pos1].connect = 1;
 		graph->edge_array[edge_pos1].weight = weight;
 
-		graph->edge_array[edge_pos2].connect = 1;
+		graph->edge_array[edge_pos2].connect = -1;
 		graph->edge_array[edge_pos2].weight = weight;
 
 	}else{
@@ -77,3 +77,4 @@ void edge_remove (ggraph * graph, unsigned id1, unsigned id2){
 	}
 
 }
+
