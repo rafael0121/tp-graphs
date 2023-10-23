f38ebd949ed9c40d9c953c1b17b7201fb1c23d91
Rafael <rafaelramosdeandrade20@gmail.com>
2023-10-23 00:23:14 -0300

    test: adiciona depth search nos testes

--- lgraphs/test/main.c
+++ lgraphs/test/main.c
@@ -18,6 +18,8 @@ void print_graph(Graph *graph){
         for(int j = 0; j < vertex->degree; j++){
             printf(" %i ->", neigh[j]);         
         }
+        
+        printf("   Grau = %d ", vertex->degree);
 
         printf("\n");
     }
@@ -26,17 +28,27 @@ void print_graph(Graph *graph){
 
 
 int main(){
-    Graph *graph;
+    Graph *graph = NULL;
     int n = 10;
 
-    graph = graph_create(graph, 10);
+    graph = graph_create(graph, 6);
     
+    edge_insert(graph, 0, 1, 0);
     edge_insert(graph, 1, 2, 0);
     edge_insert(graph, 2, 3, 0);
     edge_insert(graph, 3, 4, 0);
-    edge_insert(graph, 3, 4, 0);
-    edge_insert(graph, 3, 4, 0);
+    edge_insert(graph, 4, 5, 0);
     
+    SearchData *data = main_depth_search(graph);
+
+    for(int i=0;i<graph->total_vertex;i++){
+        
+        printf("\n\n\n%i ---------- \n", i);
+        printf("End time: %d \n", data->end_time[i]);
+        printf("Discovery time: %d \n", data->discovery_time[i]);
+        printf("Parent: %d \n", data->parent[i]);
+
+    }
 
     print_graph(graph);
     
